900a4bc8f6ce089197b7ef0c4ddc8b39d5042b1f
VictorAndres198 <andresroh98@hotmail.com>
2023-09-02 00:22:47 -0500

    Agregando styles css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar';
+import './styles.css';
 
 function App(){
     return (
-        <div>
-            <Navbar />
-            <h1>Twitter Card</h1>
+        <div>            
+            <Navbar />            
+            <h1 className='centered-text'>Iniciar el Test</h1>
+            <button className='centered-button'>Iniciar el Test</button>
         </div>
     )
 }
