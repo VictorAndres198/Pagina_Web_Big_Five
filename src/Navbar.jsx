3d353090456ba30b78cc5ace9c8f5688d8413338
Maximiliano Huasasquiche Mendoza <mhuasasquiche@sumit.com.pe>
2023-09-24 11:14:38 -0500

    Logo a Inicio

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 function CustomNavbar() {
     return (
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand as={Link} to="/Home">Logo</Navbar.Brand>
+            <Navbar.Brand as={Link} to="/">Logo</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
